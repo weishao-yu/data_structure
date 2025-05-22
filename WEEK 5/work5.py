09c032af7e184ecae6e4aad0a2901d7518bbea48
weishao-yu <yweiwei1005@gmail.com>
2025-05-22 15:28:26 +0800

    Update work5.py
    Modi

--- WEEK 5/work5.py
+++ WEEK 5/work5.py
@@ -4,11 +4,11 @@ while True:
         if n <= 1:
             print("Not prime")
         else:
-            for i in range(2, int(n**0.5) + 1):  # 检查是否有因数
+            for i in range(2, int(n**0.5) + 1):  
                 if n % i == 0:
-                    print("Not prime")  # 有因数，说明是合数
+                    print("Not prime")  
                     break
             else:
-                print("prime")  # 没有因数，说明是素数
+                print("prime") 
     except EOFError:
         break
