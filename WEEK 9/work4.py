4ef62620ccc52ec2d357b34df39b88ed1c0cea5b
weishao-yu <yweiwei1005@gmail.com>
2025-06-10 12:15:48 +0800

    modi

--- WEEK 9/work4.py
+++ WEEK 9/work4.py
@@ -1,18 +1,28 @@
 while True:
     try:
         n = int(input())
-        before = list(map(int, input().split()))
-        after = list(map(int, input().split()))
-        l, r = 0, n - 1
-        possible = True
-        for x in after:
-            if l <= r and x == before[l]:
-                l += 1
-            elif l <= r and x == before[r]:
-                r -= 1
-            else:
-                possible = False
+        A = list(map(int, input().split()))
+        B = list(map(int, input().split()))
+
+        mapping = {num: i for i, num in enumerate(A)}
+
+        pre = n + 1
+        ans = True
+        inc = False
+
+        for num in B:
+            idx = mapping[num]
+            if inc and idx < pre:
+                ans = False
                 break
-        print("Success" if possible else "Fail")
+            if idx > pre:
+                inc = True
+            pre = idx
+
+        print("Success" if ans else "Fail")
+
     except EOFError:
+        break
+    except Exception as e:
+        # 可選：print(f"輸入錯誤：{e}")
         break
